7fd8b5ae796f2914762a3baa26b55f3c84b2f1fb
Zachary Lee <w.zachary.lee@gmail.com>
2024-03-25 14:50:31 -0400

    lint

--- apps/nextjs/src/app/session/[id]/page.tsx
+++ apps/nextjs/src/app/session/[id]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/media-has-caption */
 "use client";
 
--- apps/nextjs/src/components/ui/card.tsx
+++ apps/nextjs/src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import * as React from "react";
 
 import { cn } from "~/lib/utils";
